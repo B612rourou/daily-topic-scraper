c17660cfeb9cc73b52fb3a0efbf2148815c95b4d
GitHub Action <action@github.com>
2026-02-02 23:13:08 +0800

    chore: 添加一键更新脚本

---
+++ 一键更新选题.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+set "REPO=E:\AI??\???????"
+set "GIT_EXE="
+if exist "C:\Program Files\Git\cmd\git.exe" set "GIT_EXE=C:\Program Files\Git\cmd\git.exe"
+if exist "C:\Program Files\Git\bin\git.exe" set "GIT_EXE=C:\Program Files\Git\bin\git.exe"
+if "%GIT_EXE%"=="" set "GIT_EXE=git"
+
+pushd "%REPO%" || (
+  echo ????????%REPO%
+  pause
+  exit /b 1
+)
+
+"%GIT_EXE%" rev-parse --abbrev-ref --symbolic-full-name @{u} >nul 2>&1
+if errorlevel 1 (
+  "%GIT_EXE%" branch --set-upstream-to=origin/main main >nul 2>&1
+)
+
+"%GIT_EXE%" pull
+if errorlevel 1 (
+  echo ????
+  pause
+  popd
+  exit /b 1
+)
+
+echo ????
+popd
+pause
